cdbe5a539a3d59faf3e407cf80218e47924e5679
jhay <pipzhermogenes@gmail.com>
2019-12-24 14:23:12 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Counter from './Counter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			counters: 0
+		};
+
+		this.increaseCounters = this.increaseCounters.bind(this);
+		this.decreaseCounters = this.decreaseCounters.bind(this);
+	}
+
+	render() {
+		let counters = [];
+		for (let index = 0; index < this.state.counters; index++) {
+			counters.push(<Counter key={index} counterNumber={index} />);
+		}
+
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h1 className="App-title">React Counters</h1>
+					<button onClick={this.increaseCounters}>+</button>
+					<button onClick={this.decreaseCounters}>-</button>
+				</header>
+				<div className="Counter-row">{counters}</div>
+			</div>
+		);
+	}
+
+	increaseCounters() {
+		let counters = this.state.counters + 1;
+
+		this.setState({
+			counters
+		});
+	}
+
+	decreaseCounters() {
+		let counters = this.state.counters - 1;
+
+		this.setState({
+			counters
+		});
+	}
 }
 
 export default App;
